c2e139c8736ff390673d9c3ffa93d9e3ea45a0ba
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-18 15:21:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -599,4 +599,4 @@ if('serviceWorker' in navigator) {
     .then(reg => console.log('SW registrado'))
     .catch(err => console.log('SW error:', err));
   });
-                                                 }
+  }
